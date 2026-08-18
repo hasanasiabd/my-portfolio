3607562fc0d87f4004161524c0921dbb53150c4a
hasanasiabd <hasanasiabd12@gmail.com>
2026-08-18 19:42:33 +0600

    Update route

--- data/portfolioData.js
+++ data/portfolioData.js
@@ -71,10 +71,10 @@ export const personalData = {
   resumeLink: "/resume.pdf",
 
   navLinks: [
-    { name: "Home", href: "#home" },
-    { name: "About", href: "#about" },
-    { name: "Skills", href: "#skills" },
-    { name: "Projects", href: "#projects" },
-    { name: "Contact", href: "#contact" },
+    { name: "Home", href: "/home" },
+    { name: "About", href: "/about" },
+    { name: "Skills", href: "/skills" },
+    { name: "Projects", href: "/projects" },
+    { name: "Contact", href: "/contact" },
   ]
 };
